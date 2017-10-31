31658144a7f815e088e96c5fbcd6f33b36eccd8d
LAPTOP-N23P4M16\louis <henri67000@gm>
2017-10-31 15:26:40 +0100

    update

--- PROGRAM hello world.pas
+++ PROGRAM hello world.pas
@@ -1,8 +1,19 @@
 PROGRAM hello_world;
 USES crt;
 
-BEGIN
+	CONST TVA=1.077;
+		VAR prixHT:real;
+			nbexemplaire:integer;
+			prixfinal:real;
 
-writeln ('hello world');
-readln;
-END.
+		BEGIN
+			 clrscr;
+			 writeln('prix d''un article hors taxe' );
+			 readln(prixHT);
+			 writeln('nombre d''articles');
+			 readln(nbexemplaire);
+			 prixfinal:=(prixHT*nbexemplaire)*TVA;
+			 writeln('le prix final de votre article est:',prixfinal:1:2,'euros');
+			 readkey;
+
+		END.
